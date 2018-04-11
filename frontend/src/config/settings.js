4c4a5edbf32fc622d21a58d5dfede387ed1b6095
Christopher Pole <chris@chrispole.com>
2018-04-11 13:42:12 +0100

    Update failing snapshots and fast game speed

--- frontend/src/config/settings.js
+++ frontend/src/config/settings.js
@@ -2,4 +2,4 @@ export const GRID_SIZE = 10;
 export const MIN_MOVES = 1;
 export const MAX_MOVES = 99;
 export const GAME_SPEED_NORMAL = 100;
-export const GAME_SPEED_FAST = 20;
+export const GAME_SPEED_FAST = 50;
