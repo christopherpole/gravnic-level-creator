bd3c175974a9b4fb33af8de7eb84979b7ede7f23
Christopher Pole <chris@chrispole.com>
2018-06-20 15:37:49 +0100

    Add arrows to the solution preview pane

---
+++ frontend/src/common/arrowsDisplay.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import ArrowIcon from './icons/arrowIcon';
+
+export const Wrapper = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: ${props => props.justifyContent};
+  margin: calc(-${props => props.theme.structureSpacing} / 8);
+`;
+
+export const ArrowIconWrapper = styled.li`
+  list-style-type: none;
+  max-width: ${props => props.maxArrowWidth};
+  padding: calc(${props => props.theme.structureSpacing} / 8);
+`;
+
+const ArrowsDisplay = ({ arrows, maxArrowWidth, justifyContent }) => (
+  <Wrapper justifyContent={justifyContent}>
+    {arrows.map((direction, i) => (
+      <ArrowIconWrapper maxArrowWidth={maxArrowWidth} key={i}>
+        <ArrowIcon direction={direction} />
+      </ArrowIconWrapper>
+    ))}
+  </Wrapper>
+);
+
+ArrowsDisplay.defaultProps = {
+  arrows: [],
+  maxArrowWidth: 'auto',
+  justifyContent: 'left',
+};
+
+ArrowsDisplay.propTypes = {
+  arrows: PropTypes.array,
+  maxArrowWidth: PropTypes.string,
+  justifyContent: PropTypes.string,
+};
+
+export default ArrowsDisplay;
--- frontend/src/levelSolver/components/index.js
+++ frontend/src/levelSolver/components/index.js
@@ -3,19 +3,31 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import ArrowsDisplay from 'common/arrowsDisplay';
 import SolutionStatus from './solutionStatus';
 
 export const Wrapper = styled.div`
   border: 1px solid ${props => props.theme.foregroundColor};
   margin-bottom: ${props => props.theme.structureSpacing};
   padding: ${props => props.theme.structureSpacing};
-  display: flex;
-  justify-content: center;
+  text-align: center;
 `;
 
-export const LevelSolver = solution => (
+export const ArrowsDisplayWrapper = styled.div`
+  margin: calc(${props => props.theme.structureSpacing} / 4) auto
+    calc(-${props => props.theme.structureSpacing} / 2);
+  text-align: center;
+`;
+
+export const LevelSolver = ({ solution }) => (
   <Wrapper id="level-solver">
     <SolutionStatus />
+    {solution &&
+      solution.length > 0 && (
+        <ArrowsDisplayWrapper>
+          <ArrowsDisplay justifyContent="center" maxArrowWidth="10%" arrows={solution} />
+        </ArrowsDisplayWrapper>
+      )}
   </Wrapper>
 );
 
