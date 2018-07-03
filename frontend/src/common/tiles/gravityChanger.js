ffdb1dca9a846156873cbfab0aa2de21489675a8
Christopher Pole <chris@chrispole.com>
2018-07-03 14:11:19 +0100

    Change the styling for gravity changer tiles

--- frontend/src/common/tiles/gravityChanger.js
+++ frontend/src/common/tiles/gravityChanger.js
@@ -1,66 +1,40 @@
 import React from 'react';
 import { MOVE_NONE, MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT } from 'gravnic-game';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Wrapper = styled.div`
-  background: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-
-  &:before {
-    content: '';
-    display: block;
-    width: 0;
-    height: 0;
-    border-width: 10px;
-    border-style: solid;
-    border-color: #000;
-  }
-
-  ${props => css`
-    ${props.direction === MOVE_UP &&
-      css`
-        &:before {
-          border-left-color: transparent;
-          border-right-color: transparent;
-          border-top: none;
-        }
-      `};
-
-    ${props.direction === MOVE_RIGHT &&
-      css`
-        &:before {
-          border-bottom-color: transparent;
-          border-top-color: transparent;
-          border-right: none;
-        }
-      `};
-
-    ${props.direction === MOVE_DOWN &&
-      css`
-        &:before {
-          border-left-color: transparent;
-          border-right-color: transparent;
-          border-bottom: none;
-        }
-      `};
-
-    ${props.direction === MOVE_LEFT &&
-      css`
-        &:before {
-          border-bottom-color: transparent;
-          border-top-color: transparent;
-          border-left: none;
-        }
-      `};
-  `};
+const Wrapper = styled.svg`
+  transform: rotate(${props => props.rotation}deg);
+  transform-origin: 50% 50%;
 `;
 
-const GravityChangerTile = ({ direction }) => <Wrapper direction={direction} />;
+const GravityChangerTile = ({ direction }) => (
+  <Wrapper
+    rotation={[MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT].indexOf(direction) * 90}
+    width="100%"
+    height="100%"
+    viewBox="0 0 100 100"
+    version="1.1"
+  >
+    <defs />
+    <g stroke="none" strokeWidth="0" fill="none" fillRule="evenodd">
+      <g>
+        <rect fill="#FFFFFF" x="0" y="0" width="100" height="100" />
+        {direction === MOVE_NONE && (
+          <polygon
+            stroke="#979797"
+            strokeWidth="2"
+            fill="#D8D8D8"
+            points="25 25 25 75 75 75 75 25"
+          />
+        )}
+        {direction !== MOVE_NONE && (
+          <polygon stroke="#979797" strokeWidth="2" fill="#D8D8D8" points="50 25 75 75 25 75" />
+        )}
+      </g>
+    </g>
+  </Wrapper>
+);
 
 GravityChangerTile.propTypes = {
   direction: PropTypes.oneOf([MOVE_NONE, MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT]).isRequired,
