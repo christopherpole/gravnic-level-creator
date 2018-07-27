f935f604a862fd24a76bb2634a96208a323bdde4
Christopher Pole <chris@chrispole.com>
2018-07-27 17:06:07 +0100

    Finish adding support for the crate entity

--- frontend/src/common/tiles/crate.js
+++ frontend/src/common/tiles/crate.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Wrapper = styled.div`
-  background: #f1af48;
+  transition: background ${props => props.moveSpeed}ms linear;
+  background: ${props => (props.moved ? '#ccc' : '#f1af48')};
   height: 100%;
   width: 100%;
   border: 4px solid brown;
 `;
 
-const CrateTile = () => <Wrapper />;
+const CrateTile = ({ moved, moveSpeed }) => <Wrapper moveSpeed={moveSpeed} moved={moved} />;
+
+CrateTile.defaultProps = {
+  moved: false,
+  moveSpeed: 0,
+};
+
+CrateTile.propTypes = {
+  moved: PropTypes.bool,
+  moveSpeed: PropTypes.number,
+};
 
 export default CrateTile;
